7fb8278ecd376a9ee59cc5f0f50730b6c13a602f
fallf <87919117+fallf@users.noreply.github.com>
2021-12-30 17:42:23 -0500

    update

---
+++ asset /js/forecast.js
@@ -0,0 +1,13 @@
+const key = 'c773946d61378df71adbe4fa6e9026e1'
+
+const getCity = async (city) =>{
+
+    const base = 'api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}'
+    const query = `?apikey=${key}&q=${city}`;
+
+    const response = await fetch (base + query);
+    const data = await response.json();
+    console.log(data);
+};
+
+getCity('miami');
